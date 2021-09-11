e1d387028bcb30070ab29285864970efb500628a
21prafull <21prafull@gmail.com>
2021-09-11 17:19:53 +0530

    Que2 total

--- que2.c
+++ que2.c
@@ -31,6 +31,22 @@ char* StringConcat(char* destination, const char* source){
     return destination;
 }
 
+void StringCompare(char* destination, const char* source){
+
+    int i = 0;
+    while(destination[i] != '\0' || source[i] != '\0'){
+        if(destination[i] == source[i]){
+            i++;
+        }
+        else{
+            printf("Not Equal");
+            return;
+        }
+    }
+    printf("Equal");
+    return;
+}
+
 // dest = "test123'\0'"
 // *ptr = dest + len(dest)
 // src = "source'\0'"
@@ -39,14 +55,35 @@ char* StringConcat(char* destination, const char* source){
 // print(dest) = test123source
 
 
+void StringRev(char* str){
+
+    char reverse[20] = "";
+    char* p = &reverse[0];
+    for(int i = strlen(str)-1; i>=0; i--){
+        *p = str[i];
+        p++;
+    }
+    p++;
+    *p = '\0';
+    printf("%s", reverse);
+    return;
+}
+
 
 int main()
 {
     // Stringcopy();
-    char x[100] = "";
-    StringConcat(x, "Test");
-    StringConcat(x, "Test2");    
-    printf(x);
+    // char x[100] = "";
+    // StringConcat(x, "Test");
+    // StringConcat(x, "Test2");    
+    // printf(x);
 
+    // char a[10] = "hello";
+    // char b[10] = "hello not";
+    // StringCompare(a, b);
+
+    char a[10] = "hello";
+    StringRev(a);
+    
     return 0;
 }
